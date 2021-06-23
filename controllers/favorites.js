6070a4bd823d22a1b9e6128b8f1eb1bbab69f0ed
Gunkar Singh <GunkarSingh@hotmail.com>
2021-06-23 10:51:22 -0600

    refactored code - added controller directory

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -6,18 +6,54 @@
 exports.getFavorites = (req, res, next) => {
     res.status(200).json({
         success: true,
-        action: "Show all favorites"
+        action: "Get all favorites"
+    })
+}
+
+/* 
+@desc    Get all a favorite
+@route   Get /FoodAPI/v1/favorites/:id
+@access  Public 
+*/
+exports.getFavorite = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        action: `Get favorite with id ${req.params.id}`
     })
 }
 
 /* 
 @desc    Create a new favorite
-@route   Get /FoodAPI/v1/favorites/:id
-@access  Public 
+@route   POST /FoodAPI/v1/favorites/:id
+@access  Private 
 */
 exports.createFavorite = (req, res, next) => {
     res.status(200).json({
         success: true,
         action: "Create a new favorite"
+    })
+}
+
+/* 
+@desc    Update an existing favorite
+@route   PUT /FoodAPI/v1/favorites/:id
+@access  Private 
+*/
+exports.updateFavorite = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        action: `Update an existing favorite with id ${req.params.id}`
+    })
+}
+
+/* 
+@desc    Delete an existing favorite
+@route   DELETE /FoodAPI/v1/favorites/:id
+@access  Private 
+*/
+exports.deleteFavorite = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        action: `Delete an existing favorite with id ${req.params.id}`
     })
 }
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,32 +1,20 @@
 const express = require("express")
+const {
+    getFavorites,
+    getFavorite,
+    createFavorite,
+    updateFavorite,
+    deleteFavorite
+} = require("../controllers/favorites")
+
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        action: "Show all favorites"
-    })
-})
+router.route("/").get(getFavorites).post(createFavorite)
 
-router.post("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        action: "Create a new favorite"
-    })
-})
-
-router.put("/:id", (req, res) => {
-    res.status(200).json({
-        success: true,
-        action: `Update favorite with id ${req.params.id}`
-    })
-})
-
-router.delete("/:id", (req, res) => {
-    res.status(200).json({
-        success: true,
-        action: `Delete favorite with id ${req.params.id}`
-    })
-})
+router
+    .route("/:id")
+    .get(getFavorite)
+    .put(updateFavorite)
+    .delete(deleteFavorite)
 
 module.exports = router
